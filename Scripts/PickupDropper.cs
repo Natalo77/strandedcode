a09a57ba1dc8deaa4607daef8cfea7362a5f94b9
Thirgol <jandrews15588@gmail.com>
2019-02-20 16:25:08 +0000

    Updating Dropper Scripts

--- Scripts/PickupDropper.cs
+++ Scripts/PickupDropper.cs
@@ -5,30 +5,21 @@ using UnityEngine;
 public class PickupDropper : MonoBehaviour {
 
     public GameObject[] pickups;
-    private bool itemIsThere;
     private int randomIndex;
     private int range;
-
-    public bool getItemIsThere()
-    {
-        return this.itemIsThere;
-    }
-
-    public void setItemIsThere(bool itemIsThere)
-    {
-        this.itemIsThere = itemIsThere;
-    }
 
 	// Use this for initialization
 	void Start ()
     {
-        range = Random.Range(0, 1);
-        Debug.Log(range);
+        // Choose whether to spawn pickups or not | 0 being false and 1 being true
+        range = Random.Range(0, 2);
 
-        // Spawn pickup prefabs if they are there
+        // If pickup item should spawn
         if (range == 1)
         {
-            randomIndex = Random.Range(0, 3);
+            // Choose random prefab from array
+            randomIndex = Random.Range(0, pickups.Length);
+
             // Spawn random pickup prefab from array
             GameObject newPickup = Instantiate(pickups[randomIndex], gameObject.transform.position, Quaternion.identity);
             newPickup.transform.parent = gameObject.transform;
